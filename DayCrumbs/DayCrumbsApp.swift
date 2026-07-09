b5e49b0a31978a6babc1ce1b267dd91b2d5d250f
ibnuck <masitoh7564@gmail.com>
2026-07-09 17:20:30 +0700

    Initial Commit

---
+++ DayCrumbs/DayCrumbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCrumbsApp.swift
+//  DayCrumbs
+//
+//  Created by Ibnu Taufick Ahraza on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DayCrumbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
