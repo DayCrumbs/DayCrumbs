76622f681b0bee322ff5747c7379fa29b8c4e349
RanggaRijasa <ranggarijasa2005@gmail.com>
2026-07-14 15:22:04 +0700

    Add SwiftData models and enums

---
+++ DayCrumbs/Models/CustomPlace.swift
@@ -0,0 +1,30 @@
+//
+//  CustomPlace.swift
+//  DayCrumbs
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class CustomPlace {
+    var name: String
+
+    @Attribute(.externalStorage)
+    var imageData: Data?
+
+    var recordedAt: Date
+    var dailySession: DailySession?
+
+    init(
+        name: String,
+        imageData: Data? = nil,
+        recordedAt: Date = .now,
+        dailySession: DailySession? = nil
+    ) {
+        self.name = name
+        self.imageData = imageData
+        self.recordedAt = recordedAt
+        self.dailySession = dailySession
+    }
+}
