e25e711c2e4dbce1981bd97da9a5cda9aafcf80b
Giovanny Louis <louis.anth10@gmail.com>
2026-07-14 14:05:26 +0700

    added moods

---
+++ DayCrumbs/Models/Enums/Moods.swift
@@ -0,0 +1,17 @@
+//
+//  Moods.swift
+//  DayCrumbs
+//
+//  Created by Vrz on 14/07/26.
+//
+
+import Foundation
+
+enum Mood {
+    case angry
+    case disgust
+    case fear
+    case happy
+    case sad
+    case surprise
+}
